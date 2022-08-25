f214f93510fe1481c405b1bdf767419ac8b39ae1
itc-s22025 <104543042+itc-s22025@users.noreply.github.com>
2022-08-26 00:00:57 +0900

    Edit for コメント
    idle上では日本語が打てないので、github上で直接説明を入力しました。

--- timer.py
+++ timer.py
@@ -1,3 +1,5 @@
+#　タイマー機能をつけました
+
 count = 60
 
 class Timer:
@@ -7,11 +9,14 @@ class Timer:
         self.canvas['font'] = ('Times New Roman', 20)
 
 #        self.canvas.create_text(200, 10, text=count)
+#　↑はcanvas上でcreate_textでどうにかカウントしようとしていたときの名残　ラベルのほうが楽だったのでラベルを作成しました
 
     def start(self):
+#　タイマー開始
         global count
         self.canvas['text'] = count
         count = count - 1
         self.canvas.after(1000, self.start)
         if count == 0:
             count = 'Finished!'
+#　カウントが0になったら無理やりcountの文字を'Finishd!'にする
